10a634d6d8bad7c1a044deb3591f6fd91bd74562
DionysisChristopoulos <dionysisxristopoulos98@gmail.com>
2025-03-27 11:11:46 +0000

    oversampling techniques updated

--- utils/datasets.py
+++ utils/datasets.py
@@ -6,9 +6,10 @@ import pandas as pd
 import numpy as np
 import json
 
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, SMOTENC
 from imblearn.under_sampling import RandomUnderSampler
 from ctgan import CTGAN
+from collections import Counter
 
 def labels2indices(df, label_names):
     if not isinstance(label_names, list):
@@ -86,37 +87,81 @@ def build_datasets(args, device='cpu'):
 
             X_train = torch.tensor(X_train, device=device)
             Y_train = torch.tensor(Y_train[..., np.newaxis], dtype=torch.float, device=device)
-        
+            print("Final class distribution:", Counter(Y_train.flatten().cpu().numpy()))
+
         elif args.oversampling_mode == 'ctgan':
-            train_tensor = torch.cat((X_train, Y_train), dim=-1)
-            
-            target_label = args.data.target_label
-            column_names = feature_metadata['continuous'] + list(feature_metadata['categorical'].keys())
-            column_names.append(target_label)
-            discrete_cols = column_names[len(num_indices):]
-            
-            df_train = pd.DataFrame(train_tensor.cpu().numpy(), columns=column_names)
-            df_train_positive = df_train[df_train[target_label] == 1.]
-            
-            print(f"Number of positive samples: {len(df_train_positive)}")
-            print(f"Number of discrete columns: {len(discrete_cols)}")
+                train_tensor = torch.cat((X_train, Y_train), dim=-1)
+                
+                target_label = args.data.target_label
+                column_names = feature_metadata['continuous'] + list(feature_metadata['categorical'].keys())
+                column_names.append(target_label)
+                discrete_cols = column_names[len(num_indices):]
+                
+                df_train = pd.DataFrame(train_tensor.cpu().numpy(), columns=column_names)
+                df_train_positive = df_train[df_train[target_label] == 1.]
+                
+                ctgan = CTGAN(
+                    epochs=args.ctgan.epochs,
+                    batch_size=args.ctgan.batch_size,
+                    generator_lr=args.ctgan.generator_lr,
+                    discriminator_lr=args.ctgan.discriminator_lr,
+                    generator_dim=args.ctgan.generator_dim,
+                    discriminator_dim=args.ctgan.discriminator_dim,
+                    pac=args.ctgan.pac,
+                    verbose=True, cuda=True)
+                ctgan.set_device(device)
+                ctgan.fit(df_train_positive, discrete_cols)
 
-            ctgan = CTGAN(epochs=args.ctgan.epochs, verbose=True, cuda=True)
-            ctgan.set_device(device)
-            ctgan.fit(df_train_positive, discrete_cols)
+                samples2gen = len(df_train[df_train[target_label] == 0.]) - len(df_train_positive)
+                synthetic_data = ctgan.sample(samples2gen, condition_column=target_label, condition_value=1.0)
+                synthetic_data = torch.tensor(synthetic_data.values, dtype=torch.float, device=device)
+                synthetic_data_x, synthetic_data_y = synthetic_data[:, :-1], synthetic_data[:, -1].unsqueeze(-1)
 
-            samples2gen = len(df_train[df_train[target_label] == 0.]) - len(df_train_positive)
-            print(f"Number of samples (rows) to generate: {samples2gen}")
-            synthetic_data = ctgan.sample(samples2gen, condition_column=target_label, condition_value=1.0)
-            synthetic_data = torch.tensor(synthetic_data.values, dtype=torch.float, device=device)
-            synthetic_data_x, synthetic_data_y = synthetic_data[:, :-1], synthetic_data[:, -1].unsqueeze(-1)
+                X_train = torch.cat((X_train.to(device), synthetic_data_x), dim=0)
+                Y_train = torch.cat((Y_train.to(device), synthetic_data_y), dim=0)
+                shuffled_indices = torch.randperm(X_train.size(0))
+                X_train = X_train[shuffled_indices]
+                Y_train = Y_train[shuffled_indices]
 
-            X_train = torch.cat((X_train.to(device), synthetic_data_x), dim=0)
-            Y_train = torch.cat((Y_train.to(device), synthetic_data_y), dim=0)
-            shuffled_indices = torch.randperm(X_train.size(0))
-            X_train = X_train[shuffled_indices]
-            Y_train = Y_train[shuffled_indices]
-    
+        elif args.oversampling_mode == 'smotenc':    
+
+            X_train_df = pd.DataFrame(X_train.cpu().numpy(), columns=feature_metadata['continuous'] + list(feature_metadata['categorical'].keys()))
+            # Get categorical indices
+            categorical_indices = labels2indices(X_train_df, list(feature_metadata['categorical'].keys()))
+
+            oversample = SMOTENC(categorical_features=categorical_indices,
+                                sampling_strategy=args.smotenc.oversampling_strategy,
+                                k_neighbors = args.smotenc.k_neighbors,
+                                random_state=args.train.seed)
+     
+            X_train, Y_train = oversample.fit_resample(X_train, Y_train)
+
+            if args.smotenc.undersampling:
+                assert isinstance(args.smotenc.oversampling_strategy, float), "oversampling_strategy must be a float if undersampling is enabled"
+
+                # Calculate class distribution
+                class_counts = Counter(Y_train)
+                majority_class = max(class_counts, key=class_counts.get)
+                
+                # Calculate the desired number of majority samples based on the ratio
+                ratio = args.smotenc.undersampling_strategy  # e.g., 0.9, 0.8, 0.7
+                majority_target_count = int(class_counts[majority_class] * ratio)
+
+                # Create the sampling strategy dictionary
+                sampling_strategy = {majority_class: majority_target_count}
+
+                # Apply undersampling
+                undersample = RandomUnderSampler(
+                    sampling_strategy=sampling_strategy, 
+                    random_state=args.train.seed
+                )
+                X_train, Y_train = undersample.fit_resample(X_train, Y_train)
+
+            # Convert back to tensor after resampling
+            X_train = torch.tensor(X_train, device=device)
+            Y_train = torch.tensor(Y_train[..., np.newaxis], dtype=torch.float, device=device)
+            print("Final class distribution:", Counter(Y_train.flatten().cpu().numpy()))
+
     train_dataset = TensorDataset(X_train, Y_train)
     test_dataset = TensorDataset(X_test, Y_test)
 
